4c7bbe67f4f61c5d012a9d436cd99e841500c5a9
robinwatts96 <robinwatts96@gmail.com>
2022-02-20 12:04:12 +0000

    added basic hamburger menu

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(() => {
+
+    const $hamburger = $('.hamburger');
+    const $menu = $('.hamburger-menu');
+    $hamburger.on('click', function() {
+        $hamburger.on('click', function() {
+            $menu.show();
+        });
+    });
+
+
+
+});
+
