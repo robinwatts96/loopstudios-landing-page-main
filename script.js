267ce3af8c0c0f2505e8073a61c3208504d85b61
robinwatts96 <robinwatts96@gmail.com>
2022-02-22 11:02:50 +0000

    hamburger menu complete

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 $(document).ready(() => {
 
     const $hamburger = $('.hamburger');
+    const $close = $('.hamburger-close');
     const $menu = $('.hamburger-menu');
+    const $nav = $('nav');
+
     $hamburger.on('click', function() {
         $hamburger.on('click', function() {
             $menu.show();
+            $hamburger.hide();
+            $close.show();
+            $nav.css('background-color', 'black');
         });
     });
+
+    $close.on('click', function() {
+        $menu.hide();
+        $hamburger.show();
+        $close.hide();
+        $nav.css('background-color', 'transparent');
+    })
 
 
 
